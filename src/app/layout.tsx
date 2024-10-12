5c99cb06628226794b463c6fd524be0b76d4e716
Thati05 <m.seithati01@gmail.com>
2024-10-12 12:33:00 +0200

    hero_section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={alpino.variable} >
-      <body>
+      <body className=' overflow-x-hidden bg-[#F7D210]'  >
         <Header/>
         <main>
         {children}
