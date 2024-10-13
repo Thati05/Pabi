6a12ed439b28986ec3005bf50a19e5e75181d38a
Thati05 <m.seithati01@gmail.com>
2024-10-13 10:23:10 +0200

    gsap

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,12 +1,15 @@
+"use client"
 import { asText, Content } from "@prismicio/client";
 import { PrismicRichText ,SliceComponentProps } from "@prismicio/react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
-
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
  
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
 
+gsap.registerPlugin(useGSAP)
 /**
  * Props for `Hero`.
  */
@@ -16,9 +19,55 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+
+useGSAP (() => {
+  const introTL = gsap.timeline()
+  introTL
+  .set('.hero', {opacity:1})
+  .from('.hero-header-word',{
+    scale:3,
+    opacity:0,
+    ease:"power4.in",
+    delay:0.3,
+    stagger:1,
+
+  })
+  .from(".hero_subheading",{
+    opacity:0,
+    y:30,
+  },
+  "+=.3")
+  .from(".hero-body", {
+    opacity:0,
+    y:10,
+  })
+  .from(".hero-button", {
+    opacity:0,
+    y:10,
+    duration:.7,
+  })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <Bounded
-      className="hero"
+      className="hero opacity-0 "
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
@@ -27,7 +76,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center ">
 
-      <h1 className=" hero-header text-center text-[#FFF9C7]  lg:text-[150px] text-7xl font-black uppercase leading-[.8] md:text-[144px] ">
+      <h1 className=" hero-header text-center text-[#FFF9C7]  lg:text-[150px] text-9xl font-black uppercase leading-[.8] md:text-[144px] ">
 
    <TextSplitter text={asText(slice.primary.heading)} wordDisplayStyle="block" className="hero-header-word"   /> {/* fixes the hydration error*/}
       </h1>
